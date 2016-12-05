5fe161916920e37d6a1d1c31def57859f0759fc8
SawhneyVardan <VardanSawhney@vardans-macbook-pro.local>
2016-12-04 22:50:10 -0500

    Initial Commit

---
+++ Cancer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cancer
+//
+//  Created by Vardan Sawhney on 2016-12-04.
+//  Copyright © 2016 Vardan Sawhney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
